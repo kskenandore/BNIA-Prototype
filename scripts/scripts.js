e751715f1f49ca2aab063b23b0096029c581e2ff
Keira <keiraskenandore@gmail.com>
2019-11-11 19:32:41 -0600

    added skip profile and added search bar function

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -14,3 +14,23 @@ function selectTopic(id) {
         topic.classList.add("selected");
     }
 }
+
+function searchingNeighborhoods() {
+    // Declare variables
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById('neighborhoodSearch');
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("findNeighborhoods");
+    li = ul.getElementsByTagName('li');
+
+    // Loop through all list items, and hide those who don't match the search query
+    for (i = 0; i < li.length; i++) {
+        label = li[i].getElementsByTagName("label")[0];
+        txtValue = label.textContent || label.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        li[i].style.display = "";
+        } else {
+        li[i].style.display = "none";
+        }
+    }
+  }
