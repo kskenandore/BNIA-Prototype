4a2eae45a856f3830d07cf2e6c34dff198e602e9
Keira <keiraskenandore@gmail.com>
2019-11-12 21:59:59 -0600

    fixed data page to look like design

--- scripts/maps.js
+++ scripts/maps.js
@@ -3,12 +3,27 @@ var mymap = L.map('mapid', {
     zoom: 13
 });
 
-
 var mapbox = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
     maxZoom: 20,
     accessToken: 'pk.eyJ1Ijoia3NrZW5hbmRvcmUiLCJhIjoiY2syd2NwcWVsMDQzMjNqcHFnbDkzdzA3cCJ9.L04N0CzzlY0T4sOmE-96dg'
 }).addTo(mymap);
 
+//L.geoJSON( cData ).addTo(mymap);
+
 function resizeMap() {
     mymap.invalidateSize()
+}
+
+var datamap = L.map('data-map', {
+    center: [39.3214, -76.5757],
+    zoom: 10
+});
+
+var datamapbox = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v10/tiles/256/{z}/{x}/{y}?access_token={accessToken}', {
+    maxZoom: 10,
+    accessToken: 'pk.eyJ1Ijoia3NrZW5hbmRvcmUiLCJhIjoiY2syd2NwcWVsMDQzMjNqcHFnbDkzdzA3cCJ9.L04N0CzzlY0T4sOmE-96dg'
+}).addTo(datamap);
+
+function resizeDataMap() {
+    datamap.invalidateSize()
 }
